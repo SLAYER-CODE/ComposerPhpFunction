25ac652785421f3d691c66b465d53605da3aa01a
Edwin Paz <1edw333@gmail.com>
2022-11-12 14:21:46 -0500

    Primera vercion para PHP

---
+++ src/index.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.cdnfonts.com/css/matematica" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Get Content Files 'PDF' Archives</h1>
+</body>
+</html>
+<?php
+    $start_time = microtime(true);
+    include "../vendor/autoload.php";
+
+
+    function secondsToTime($s)
+    {
+        $h = floor($s / 3600);
+        $s -= $h * 3600;
+        $m = floor($s / 60);
+        $s -= $m * 60;
+        return $h.':'.sprintf('%02d', $m).':'.sprintf('%02d', $s);
+    }
+
+    $start_time=microtime(true);    
+    $parseador = new \Smalot\PdfParser\Parser();
+    $PathDirAbsolute="D:\\";
+    $VarItem = scandir($PathDirAbsolute);
+    $VarArrayFilters=array();
+    foreach ($VarItem as $item){
+        $extencion = (new SplFileInfo($item))->getExtension();
+        if($extencion == 'pdf' && !strpos($item,'$RECYCLE.BIN')) {
+            array_push($VarArrayFilters,$item); 
+            echo "<h5>$item</h5>";
+        }
+    }
+
+    foreach($VarArrayFilters as $documento){
+        try{
+        $doc = $parseador->parseFile($PathDirAbsolute.$documento);
+        }catch (Exception $e){
+            echo "Ubo un error $documento".$e;
+        }
+        $paginas = $doc->getPages();
+        $title = $doc->getDetails();
+        echo "<h1>$documento</h1>";
+        print_r($title);
+        echo "<div class='divTextPdf'>";
+        foreach($paginas as $indice=>$pagina){
+            $texto = $pagina->getText();
+            $indice += 1;
+            echo "<div>";
+            echo "<h2 class='indiceText'> $indice</h2>";
+            echo "<textarea class='text' rows='20' cols='10'> $texto </textarea>";
+            echo "</div>";
+        }
+        echo "</div>";
+    }
+    $end_time = microtime(true);
+    $duration= secondsToTime($end_time -$start_time);
+    echo "<h1>$duration</h1>"    
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'javleds/fabulosa-libreria',
+        'pretty_version' => '1.0',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'javleds/fabulosa-libreria' => array(
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'smalot/pdfparser' => array(
+            'pretty_version' => 'v2.2.1',
+            'version' => '2.2.1.0',
+            'reference' => '7c007090776f29119e72be28f7780e638ea0cbf4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../smalot/pdfparser',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-mbstring' => array(
+            'pretty_version' => 'v1.27.0',
+            'version' => '1.27.0.0',
+            'reference' => '8ad114f6b39e2c98a8b0e3bd907732c207c2b534',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/polyfill-mbstring',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
